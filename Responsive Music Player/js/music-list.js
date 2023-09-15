88eaf540b5181d7ee7e3563642fa9c2f86ed5807
Kanish <111358462+kanish13@users.noreply.github.com>
2023-09-15 18:05:32 +0530

    Add files via upload

---
+++ Responsive Music Player/js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Fur Elise",
+    artist: "Beethoven",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Intersellar",
+    artist: "Hans Zimmer",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Lux Aterna",
+    artist: "Clint Mansell",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Lose Yourself",
+    artist: "Eminem",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Moonlight Sonata",
+    artist: "Beethoven",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Radioactive",
+    artist: "Imagine dragons",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
